7a6e3194d64631b6cf4f059a3d3102b5a7c40b7a
github username <github email address>
2019-10-14 16:49:14 -0400

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
   const form = document.getElementById('joke-form')
-  const jokeList = document.getElementById('joke-list')
-  const newJokeLi = document.createElement('li')
-  const username = document.getElementById('name-input').value
-  let joke;
-
+  
   function fetchJoke(){
-    fetch('https://icanhazdadjoke.com/', {
+    return fetch('https://icanhazdadjoke.com/', {
       headers: {
         "Accept": "application/json"
       }
     })
     .then(res => res.json())
-    .then(jokeData => joke = jokeData.joke)
+    .then(jokeData => jokeData.joke )
   }
 
   form.addEventListener('submit', (event) => {
-
-    if(username === "") return;
-    fetchJoke()
-    newJokeLi.innerHTML = `
-    <span class="username">${username} says:</span> ${joke}
-    `
-    jokeList.appendChild(newJokeLi)
+    event.preventDefault();
+    const jokeList = document.getElementById('joke-list')
+    const newJokeLi = document.createElement('li')
+    const username = document.getElementById('name-input').value
+    
+    if(username === "") return; 
+      
+    fetchJoke().then(joke => {
+      newJokeLi.innerHTML = `
+      <span class="username">${username} says:</span> ${joke}
+      `
+      jokeList.appendChild(newJokeLi)
+    })
   })
 })
+
+// USER EXPERIENCE - DEBUGGING!!!
+//  1. JOKES SHOULD PERSIST ON THE PAGE
+//  2. NAME INPUT SHOULD BE ACCEPTED 
